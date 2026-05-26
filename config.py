2c16b6dcb8129628df9099eb597f9e27efb43d09
kichu <kichuxpro@outlook.com>
2026-05-26 20:42:51 +0530

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-# Fill these with your actual credentials
-#@cantarellabots
-API_ID=""
-API_HASH=""
-BOT_TOKEN=""
-MONGO_URI="mongodb+srv://jiosaavn:jiosaavn@cluster0.ouhhe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-OWNER_ID=""
