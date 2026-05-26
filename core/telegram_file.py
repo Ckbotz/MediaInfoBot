f576437f84d570daa20617c625608b808a4dfd5a
kichu <kichuxpro@outlook.com>
2026-05-26 20:51:49 +0530

    Change local URL port from 8080 to 8000

--- core/telegram_file.py
+++ core/telegram_file.py
@@ -3,5 +3,5 @@ async def get_file_url(chat_id, message_id):
     Returns a local URL that our background streamer will handle.
     This bypasses the 20MB Bot API limit.
     """
-    return f"http://127.0.0.1:8080/stream/{chat_id}/{message_id}"
+    return f"http://127.0.0.1:8000/stream/{chat_id}/{message_id}"
 #@cantarellabots
