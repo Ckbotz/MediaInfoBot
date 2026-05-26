f21169ea402dd08c0333fafa020019f39d119a9b
kichu <kichuxpro@outlook.com>
2026-05-26 18:55:33 +0530

    Initialize API credentials in config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Fill these with your actual credentials
 #@cantarellabots
-API_ID =
-API_HASH =
-BOT_TOKEN =
-MONGO_URI =
-OWNER_ID =
+API_ID=""
+API_HASH=""
+BOT_TOKEN=""
+MONGO_URI=""
+OWNER_ID=""
